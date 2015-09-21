687269004bb989b9883aa6c280e0b99c20208428
LenAnderson <anderson.len@outlook.com>
2015-09-21 15:07:52 +0200

    Initial Commit

---
+++ WallpaperChanger/Preferences.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace WallpaperChanger
+{
+    partial class Preferences
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Preferences));
+            this.cbxWallpaperStyle = new System.Windows.Forms.ComboBox();
+            this.systray = new System.Windows.Forms.NotifyIcon(this.components);
+            this.lblWallpaperStyle = new System.Windows.Forms.Label();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnRescan = new System.Windows.Forms.Button();
+            this.grpRescan = new System.Windows.Forms.GroupBox();
+            this.lblRescan = new System.Windows.Forms.Label();
+            this.grpRescan.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cbxWallpaperStyle
+            // 
+            this.cbxWallpaperStyle.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxWallpaperStyle.FormattingEnabled = true;
+            this.cbxWallpaperStyle.Items.AddRange(new object[] {
+            "Fill",
+            "Fit",
+            "Stretch",
+            "Tile",
+            "Center"});
+            this.cbxWallpaperStyle.Location = new System.Drawing.Point(99, 12);
+            this.cbxWallpaperStyle.Name = "cbxWallpaperStyle";
+            this.cbxWallpaperStyle.Size = new System.Drawing.Size(121, 21);
+            this.cbxWallpaperStyle.TabIndex = 0;
+            // 
+            // systray
+            // 
+            this.systray.BalloonTipText = "Wallpaper Changer";
+            this.systray.BalloonTipTitle = "WC";
+            this.systray.Icon = ((System.Drawing.Icon)(resources.GetObject("systray.Icon")));
+            this.systray.Text = "notifyIcon1";
+            this.systray.Visible = true;
+            this.systray.DoubleClick += new System.EventHandler(this.systray_DoubleClick);
+            // 
+            // lblWallpaperStyle
+            // 
+            this.lblWallpaperStyle.AutoSize = true;
+            this.lblWallpaperStyle.Location = new System.Drawing.Point(12, 15);
+            this.lblWallpaperStyle.Name = "lblWallpaperStyle";
+            this.lblWallpaperStyle.Size = new System.Drawing.Size(81, 13);
+            this.lblWallpaperStyle.TabIndex = 1;
+            this.lblWallpaperStyle.Text = "Wallpaper Style";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.Location = new System.Drawing.Point(172, 162);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnOk
+            // 
+            this.btnOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOk.Location = new System.Drawing.Point(91, 162);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 3;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // btnRescan
+            // 
+            this.btnRescan.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnRescan.Location = new System.Drawing.Point(9, 65);
+            this.btnRescan.Name = "btnRescan";
+            this.btnRescan.Size = new System.Drawing.Size(75, 23);
+            this.btnRescan.TabIndex = 4;
+            this.btnRescan.Text = "Rescan";
+            this.btnRescan.UseVisualStyleBackColor = true;
+            this.btnRescan.Click += new System.EventHandler(this.btnRescan_Click);
+            // 
+            // grpRescan
+            // 
+            this.grpRescan.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.grpRescan.Controls.Add(this.lblRescan);
+            this.grpRescan.Controls.Add(this.btnRescan);
+            this.grpRescan.Location = new System.Drawing.Point(12, 39);
+            this.grpRescan.Name = "grpRescan";
+            this.grpRescan.Size = new System.Drawing.Size(235, 94);
+            this.grpRescan.TabIndex = 5;
+            this.grpRescan.TabStop = false;
+            this.grpRescan.Text = "Rescan";
+            // 
+            // lblRescan
+            // 
+            this.lblRescan.AutoSize = true;
+            this.lblRescan.Location = new System.Drawing.Point(6, 16);
+            this.lblRescan.MaximumSize = new System.Drawing.Size(215, 0);
+            this.lblRescan.Name = "lblRescan";
+            this.lblRescan.Size = new System.Drawing.Size(213, 39);
+            this.lblRescan.TabIndex = 0;
+            this.lblRescan.Text = "Scan the \"wallpapers\" folder for new files and subfolders if you have added new c" +
+    "ontent after launching Wallpaper Changer.";
+            // 
+            // Preferences
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(259, 197);
+            this.Controls.Add(this.grpRescan);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.lblWallpaperStyle);
+            this.Controls.Add(this.cbxWallpaperStyle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(265, 225);
+            this.Name = "Preferences";
+            this.Text = "Wallpaper Changer";
+            this.Resize += new System.EventHandler(this.Preferences_Resize);
+            this.grpRescan.ResumeLayout(false);
+            this.grpRescan.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbxWallpaperStyle;
+        private System.Windows.Forms.NotifyIcon systray;
+        private System.Windows.Forms.Label lblWallpaperStyle;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnRescan;
+        private System.Windows.Forms.GroupBox grpRescan;
+        private System.Windows.Forms.Label lblRescan;
+
+
+
+    }
+}
+
---
+++ WallpaperChanger/Preferences.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+using System.Management;
+using System.IO;
+using System.Collections.Generic;
+
+namespace WallpaperChanger
+{
+    public partial class Preferences : Form
+    {
+        String[] wallpapers;
+        String currentProcess;
+        String wallpaperStyle;
+        Dictionary<String, String> wallpaperStyles;
+
+        public Preferences()
+        {
+            InitializeComponent();
+
+            wallpaperStyles = new Dictionary<String, String>();
+            wallpaperStyles.Add("Fill", "10");
+            wallpaperStyles.Add("Fit", "6");
+            wallpaperStyles.Add("Stretch", "2");
+            wallpaperStyles.Add("Tile", "0");
+            wallpaperStyles.Add("Center", "0");
+
+            cbxWallpaperStyle.Text = Properties.Settings.Default.wallpaperStyle;
+            wallpaperStyle = cbxWallpaperStyle.Text;
+
+            scanWallpapers();
+
+            ManagementEventWatcher startWatch = new ManagementEventWatcher(new WqlEventQuery("SELECT * FROM Win32_ProcessStartTrace"));
+            startWatch.EventArrived += new EventArrivedEventHandler(startWatch_EventArrived);
+            startWatch.Start();
+
+            ManagementEventWatcher stopWatch = new ManagementEventWatcher(new WqlEventQuery("SELECT * FROM Win32_ProcessStopTrace"));
+            stopWatch.EventArrived += new EventArrivedEventHandler(stopWatch_EventArrived);
+            stopWatch.Start();
+        }
+
+        private void scanWallpapers()
+        {
+            wallpapers = Directory.GetFiles(@"wallpapers\", "*.jpg").Concat(Directory.GetDirectories(@"wallpapers\")).ToArray();
+        }
+
+        void stopWatch_EventArrived(object sender, EventArrivedEventArgs e)
+        {
+            if (e.NewEvent.Properties["ProcessName"].Value.ToString() == currentProcess && wallpapers.Contains(@"wallpapers\default.jpg"))
+            {
+                currentProcess = "";
+                setWallpaper(@"wallpapers\default.jpg");
+            }
+        }
+
+        void startWatch_EventArrived(object sender, EventArrivedEventArgs e)
+        {
+            String process = e.NewEvent.Properties["ProcessName"].Value.ToString();
+            if (wallpapers.Contains(@"wallpapers\" + process + ".jpg"))
+            {
+                currentProcess = process;
+                setWallpaper(@"wallpapers\" + process + ".jpg");
+            }
+            else if (wallpapers.Contains(@"wallpapers\" + process))
+            {
+                String[] subpapers = Directory.GetFiles(@"wallpapers\" + process, "*.jpg");
+                if (subpapers.Count() > 0)
+                {
+                    currentProcess = process;
+                    setWallpaper(subpapers[new Random().Next(subpapers.Count())]);
+                }
+                
+            }
+        }
+
+        private void setWallpaper(String path)
+        {
+            Registry.SetValue("HKEY_CURRENT_USER\\Control Panel\\Desktop", "WallpaperStyle", wallpaperStyles[wallpaperStyle]);
+            if (wallpaperStyle == "Tile")
+            {
+                Registry.SetValue("HKEY_CURRENT_USER\\Control Panel\\Desktop", "TileWallpaper", "1");
+            }
+            else
+            {
+                Registry.SetValue("HKEY_CURRENT_USER\\Control Panel\\Desktop", "TileWallpaper", "0");
+            }
+            SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, Path.GetFullPath(path), SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+        }
+
+        private void EnableActiveDesktop()
+        {
+            IntPtr result = IntPtr.Zero;
+            // enable active desktop
+            SendMessageTimeout(FindWindow("Progman", IntPtr.Zero), 0x52c, IntPtr.Zero, IntPtr.Zero, 0, 500, out result);
+
+
+        }
+
+
+        private void btnRescan_Click(object sender, EventArgs e)
+        {
+            scanWallpapers();
+        }
+
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            wallpaperStyle = cbxWallpaperStyle.Text;
+            Properties.Settings.Default.wallpaperStyle = cbxWallpaperStyle.Text;
+            Properties.Settings.Default.Save();
+            this.WindowState = FormWindowState.Minimized;
+        }
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            cbxWallpaperStyle.Text = wallpaperStyle;
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+
+        private void Preferences_Resize(object sender, EventArgs e)
+        {
+            if (FormWindowState.Minimized == this.WindowState)
+            {
+                this.Hide();
+            }
+        }
+
+        private void systray_DoubleClick(object sender, EventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+
+
+
+        const int SPI_SETDESKWALLPAPER = 20;
+        const int SPIF_UPDATEINIFILE = 0x01;
+        const int SPIF_SENDWININICHANGE = 0x02;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int SendMessageTimeout(
+            IntPtr hWnd,      // handle to destination window
+            uint Msg,       // message
+            IntPtr wParam,  // first message parameter
+            IntPtr lParam,   // second message parameter
+            uint fuFlags, uint uTimeout, out IntPtr result);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr FindWindow(string lpClassName, IntPtr ZeroOnly);
+    }
+}
